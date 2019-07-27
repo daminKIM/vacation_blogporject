f47c8e88e0414e08699b890d01ca9221a980e1a3
daminKIM <ekals4407@gmail.com>
2019-07-27 22:10:54 +0900

    last

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1!rm5=)x4$e&7f-en39ly=b6d(_nryf#7cw&_=ql!69+fl8xw9'
+#SECRET_KEY = '1!rm5=)x4$e&7f-en39ly=b6d(_nryf#7cw&_=ql!69+fl8xw9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
